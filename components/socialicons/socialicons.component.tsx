c45965bd4d48a71746f6b8d9e45ea03d791739bb
tjuranek <thomas@juranek.com>
2022-03-21 13:19:50 -0500

    update social icon colors and links

--- components/socialicons/socialicons.component.tsx
+++ components/socialicons/socialicons.component.tsx
@@ -5,10 +5,21 @@ import styles from './socialicons.module.css';
 export function SocialIcons() {
     return (
         <ButtonGroup>
-            <FiInstagram className={styles.icon} />
-            <FiTwitter className={styles.icon} />
-            <FiLinkedin className={styles.icon} />
-            <FiGithub className={styles.icon} />
+            <a href="https://www.instagram.com/tjuranek/" target="_blank" rel="noreferrer">
+                <FiInstagram className={`${styles.icon} ${styles.instagram}`} />
+            </a>
+
+            <a href="https://twitter.com/thomas_juranek" target="_blank" rel="noreferrer">
+                <FiTwitter className={`${styles.icon} ${styles.twitter}`} />
+            </a>
+
+            <a href="https://www.linkedin.com/in/thomasjuranek/" target="_blank" rel="noreferrer">
+                <FiLinkedin className={`${styles.icon} ${styles.linkedin}`} />
+            </a>
+
+            <a href="https://github.com/tjuranek" target="_blank" rel="noreferrer">
+                <FiGithub className={`${styles.icon} ${styles.github}`} />
+            </a>
         </ButtonGroup>
     );
 };
