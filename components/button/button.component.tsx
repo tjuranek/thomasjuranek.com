a8e0308ca7dae11ee3817cf10ade539d88b01de3
tjuranek <thomas@juranek.com>
2022-03-20 21:08:33 -0500

    restructure button component files

--- components/button/button.component.tsx
+++ components/button/button.component.tsx
@@ -1,15 +1,16 @@
 import styles from './button.module.css';
 
 export function Button(props: Props) {
-    const { label } = props;
+    const { additionalClassNames, label } = props;
 
     return (
-        <button className={styles.button}>
+        <button className={`${styles.button} ${additionalClassNames}`}>
             {label}
         </button>
     );
 };
 
 interface Props {
+    additionalClassNames?: string;
     label: string;
 }
