705caa1b033ede2bcef34bca96ca13ed275c8b82
tjuranek <thomas@juranek.com>
2022-03-21 12:14:39 -0500

    add meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="theme-color" content="#121212" />
+                <meta name="description" content="Minnesota based web developer. Passionate about efficiently building lovable products. Enjoys getting outdoors and to the gym." />
+                <meta name="keywords" content="Thomas Juranek, Nisswa, Brainerd, Baxter, Web Developer" />
+
+                <link rel="canonical" href="https://thomasjuranek.com" />
+
+                <title>Thomas Juranek</title>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
