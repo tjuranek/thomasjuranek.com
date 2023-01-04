ee64b7e70171efaf3016455f31bd851fd8f2ea00
tjuranek <thomas@juranek.com>
2023-01-03 19:03:26 -0700

    add supabase for collecting emails

---
+++ src/db/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function addEmailToNewsletter(name, email) {
+  return await supabase.from('subscribers').insert({ name, email })
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -110,26 +110,47 @@ function SocialLink({ icon: Icon, ...props }) {
 function Newsletter() {
   return (
     <form
-      action="/thank-you"
+      action="/api/subscribe"
+      method="post"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
+        <span className="ml-3">Join My Newsletter</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
+        My goal is to publish a new post on the first Wednesday of every month.
+        Sign up to get notified of when I publish something new, and unsubscribe
+        at any time.
       </p>
+
       <div className="mt-6 flex">
         <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name"
+          aria-label="Name"
+          required
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+        />
+      </div>
+
+      <div className="mt-2 flex">
+        <input
+          id="email"
+          name="email"
           type="email"
           placeholder="Email address"
           aria-label="Email address"
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
+      </div>
+
+      <div className="mt-6 flex">
+        <Button type="submit" className="flex-none">
+          Subscribe
         </Button>
       </div>
     </form>
